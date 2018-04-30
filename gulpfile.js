f4c4aff7fcc017d5c7dd37b6d6a44ba4363c531c
celsom <celsomanhique97@gmail.com>
2018-04-30 17:46:40 +0200

    Conacatenated javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat =  require('gulp-concat');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var jsSources =[
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+
+
+];
 
 
 gulp.task('coffee', function(){
@@ -15,4 +25,11 @@ gulp.task('coffee', function(){
 			
 
 
+});
+
+
+gulp.task('js',function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
